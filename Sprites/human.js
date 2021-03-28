ffa69434e87325cb4767170b341ef5ceca9bf3d0
Martin <martidobrev33@gmail.com>
2021-03-28 13:12:43 +0300

    SAS:4

--- Sprites/human.js
+++ Sprites/human.js
@@ -5,31 +5,30 @@ let human = new blockLike.Sprite({
     image: 'human (1).png'
 });
 human.addTo(stage);
-human.setRotationStyle('left-right');
-human.pointInDirection(-90);
-
 human.whenKeyPressed('d', function () {// d
     if(this.x < stage.width/2-100){
         this.changeX(100);
-    this.pointInDirection(-90);
+        this.pointInDirection(30);
     }
 });
 
 human.whenKeyPressed('a', function () {// a
     if(this.x > (stage.width / 2 * -1) +100){
-      this.changeX (-100)
-    this.pointInDirection(90);
+      this.changeX (-100);
+      this.pointInDirection(-150);
     }
 });
 
 human.whenKeyPressed('w', function () {// w
     if(this.y < stage.height /2-100){
        this.changeY (100)
+       this.pointInDirection(-90);
     }
 });
 
 human.whenKeyPressed('s', function () {// s
     if(this.y > (stage.height / 2 * -1) +100){
        this.changeY (-100)
+       this.pointInDirection(90);
     }
 });
--- Sprites/zombie.js
+++ Sprites/zombie.js
@@ -2,34 +2,33 @@ let zombie = new blockLike.Sprite({
     width: 100,
     height: 100,
     color: 'transparent',
-    image: 'zombie (2).png'
+    image: 'zombie.png'
 });
 zombie.addTo(stage);
-zombie.setRotationStyle('left-right');
-zombie.pointInDirection(-90);
-
 zombie.whenKeyPressed(39, function () {// right arrow
     if(this.x < stage.width/2-100){
        this.changeX(100);
-    this.pointInDirection(-90);
+       this.pointInDirection(30);
     }
 });
 
 zombie.whenKeyPressed(37, function () {// left arrow
     if(this.x > (stage.width / 2 * -1) +100){
        this.changeX (-100)
-    this.pointInDirection(90);
+       this.pointInDirection(-150);
     }
 });
 
 zombie.whenKeyPressed(38, function () {// up arrow 
     if(this.y < stage.height /2-100){
        this.changeY (100)
+       this.pointInDirection(-90);
     }
 });
 
 zombie.whenKeyPressed(40, function () {// down arrow
     if(this.y > (stage.height / 2 * -1) +100){
        this.changeY (-100)
+       this.pointInDirection(90);
     }
 });
