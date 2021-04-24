df4897820d0ada1f246eb809b1220ccdb43e256a
Martin <martidobrev33@gmail.com>
2021-04-25 01:00:00 +0300

    sas 4

--- Sprites/zombie.js
+++ Sprites/zombie.js
@@ -4,16 +4,9 @@ let zombie = new blockLike.Sprite({
     color: 'transparent',
     image: 'zombie.png'
 });
+
 zombie.addTo(stage);
 zombie.setX(500)
-zombie.whenKeyPressed(39, function () {// right arrow
-    if(this.x < stage.width/2-100){
-       this.changeX(100);
-       this.pointInDirection(30);
-       isGameOver();
-    }
-});
-
 zombie.whenKeyPressed(37, function () {// left arrow
     if(this.x > (stage.width / 2 * -1) +100){
        this.changeX (-100)
@@ -21,7 +14,6 @@ zombie.whenKeyPressed(37, function () {// left arrow
        isGameOver();
     }
 });
-
 zombie.whenKeyPressed(38, function () {// up arrow 
     if(this.y < stage.height /2-100){
        this.changeY (100)
@@ -34,6 +26,6 @@ zombie.whenKeyPressed(40, function () {// down arrow
     if(this.y > (stage.height / 2 * -1) +100){
        this.changeY (-100)
        this.pointInDirection(90);
-       isGameOver();
-    }
-});
+        isGameOver();
+   }
+    });
