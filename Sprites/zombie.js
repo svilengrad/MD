dfa1d18f7a3cb2999061c971f3f28f5aec3b2b0a
Martin <martidobrev33@gmail.com>
2021-06-20 13:29:41 +0300

    sas

--- Sprites/zombie.js
+++ Sprites/zombie.js
@@ -4,33 +4,53 @@ let zombie = new blockLike.Sprite({
     color: 'transparent',
     image: 'zombie.png'
 });
+
+// Initialisation
+
 zombie.addTo(stage);
-zombie.setX(500)
-zombie.whenKeyPressed(37, function () {// left arrow
+zombie.hide();
+
+// Game Start
+
+stage.whenReceiveMessage("game-start", function(){
+    zombie.setX(500)
+    zombie.show();
+});
+
+// Key Controls
+
+// left arrow
+zombie.whenKeyPressed(37, function () {
     if(this.x > (stage.width / 2 * -1) +100){
        this.changeX (-100)
        this.pointInDirection(-150);
        isGameOver();
     }
 }); 
-zombie.whenKeyPressed(39, function () {// right arrow
+
+// right arrow
+zombie.whenKeyPressed(39, function () {
     if(this.x < stage.width/ 2-100){
         this.pointInDirection(360);
         this.changeX(100);
         isGameOver();
     }
 });
-zombie.whenKeyPressed(38, function () {// up arrow 
+
+// up arrow 
+zombie.whenKeyPressed(38, function () {
     if(this.y < stage.height /2-100){
        this.changeY (100)
        this.pointInDirection(-90);
        isGameOver();
     }
 });
-zombie.whenKeyPressed(40, function () {// down arrow
+
+// down arrow
+zombie.whenKeyPressed(40, function () {
     if(this.y > (stage.height / 2 * -1) +100){
        this.changeY (-100)
        this.pointInDirection(90);
         isGameOver();
    }
-    });
+});
